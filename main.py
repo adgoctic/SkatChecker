199f237952f3d3e452aff84d06a0b1f58cd8383f
adgoctic <emil.obert@t-online.de>
2020-12-06 21:58:57 +0100

    (possibly) implemented processUserInput

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 # Copyright: Emil Obert, 2020
 # Author:    Emil Obert <emil.obert@t-online.de>
 
-
+#ToDo: add random card array generator for monte carlo analysis
 
 class Card:
     def __init__(self, value = '', suit = ''):
         self.value = value
         self.suit = suit
+
+
+# takes 60(or less)-char string, converts to custom data type with attributes
+# !!!input syntax: value first, then suit (like english pronounciation)
+# returns: card array to be processed by checker class
+def processUserInput(input):
+    #
+    cardArray = []
+    # create a Card object for every card and add it to array
+    for index in enumerate(input):
+        if index % 2 is 0:
+            cardArray.append(Card(input[index], input[index + 1]))
+        else:
+            continue
+    return cardArray
 
 
 class Checker:
@@ -16,12 +31,6 @@ class Checker:
 
 
 
-# takes 60(or less)-char string, converts to custom data type with attributes
-# !!!input syntax: value first, then suit (like english pronounciation)
-def processUseInput(input):
-    #
-    for (i = 0; i < 59; i += 2)
 
-    if __name__ == '__main__':
-        print_hi('PyCharm')
-
+if __name__ == '__main__':
+    processUserInput('')
