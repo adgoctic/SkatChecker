8b42f2d432421d4db06407129e9be8e26914964c
adgoctic <emil.obert@t-online.de>
2020-12-07 15:30:54 +0100

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Copyright: Emil Obert, 2020
 # Author:    Emil Obert <emil.obert@t-online.de>
 
-#ToDo: add random card array generator for monte carlo analysis
+# ToDo: add random card array generator for monte carlo analysis
 
 class Card:
     def __init__(self, value = '', suit = ''):
@@ -9,28 +9,28 @@ class Card:
         self.suit = suit
 
 
-# takes 60(or less)-char string, converts to custom data type with attributes
-# !!!input syntax: value first, then suit (like english pronounciation)
-# returns: card array to be processed by checker class
-def processUserInput(input):
-    #
-    cardArray = []
-    # create a Card object for every card and add it to array
-    for index in enumerate(input):
-        if index % 2 is 0:
-            cardArray.append(Card(input[index], input[index + 1]))
-        else:
-            continue
-    return cardArray
 
 
-class Checker:
+class Checker:  # ???? no class???
     cards = []
     def __init__(self, cardArray = []):
         self.cards = cardArray
 
 
+    # takes 60(or less)-char string, converts to custom data type with attributes
+    #   ->maybe change to list of 30 2-char strings(?)
+    # !!!input syntax: value first, then suit (like english pronounciation)
+    # returns: card array to be processed by checker class
+    def processUserInput(input):
+        #
+        cardArray = []
+        # create a Card object for every card and add it to array
+        for index in enumerate(input):
+            if index % 2 is 0:
+                cardArray.append(Card(input[index], input[index + 1]))
+            else:
+                continue
+        return cardArray
 
 
-if __name__ == '__main__':
-    processUserInput('')
+# if __name__ == '__main__':
