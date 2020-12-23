8bba0b017fedf9adf7ef62425e72e1651ef740cc
adgoctic <emil.obert@t-online.de>
2020-12-23 18:24:09 +0100

    added order-determination for zero-game (not tested :P)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Card:
 
 
 class Checker:
-    cards = []
+    # cards = []
     def __init__(self, cardArray = []):
         self.cards = cardArray
 
@@ -32,26 +32,50 @@ class Checker:
                 continue
         return cardArray
 
-    dict_zero = {'7' : 0,
-                 '8' : 1,
-                 '9' : 2,
-                 '0' : 3,
-                 'J' : 4,
-                 'Q' : 5,
-                 'K' : 6
-                 'A' : 7}
 
-    # takes: gametype (ToDo: represent by number (?)
+
+    # takes: gametype (represented by number (?))
     #       ToDo / idea: no argument, always all types, return all orders at once ( might add unneccesary runtime)
     # returns: array of 9 numbers, representing the relative position of starting player for every
     # round, first round is always player 1 (implicit, left out)
-    def getOrder(gametype):
+    def getOrder(self, gametype):
+        player_order = []
         # switch case for game types (?)
         # outsource the 4 color options to another function ? (no quadruple code)
-
         # for zero game (no trump)
         if gametype is 0:
-            for i in range(cards.)
+            dict_zero = {'7': 0,
+                         '8': 1,
+                         '9': 2,
+                         '0': 3,
+                         'J': 4,
+                         'Q': 5,
+                         'K': 6,
+                         'A': 7}
+            for i in range(9):
+                # remember first-played card of this round
+                first_card = self.cards[i * 3]
+                suit_to_match = first_card.suit
+                highest_value = dict_zero[first_card.value]
+
+                # set 1st player as default for next starting player
+                next_starting_player = 0  # offset ( either 0,1 or 2)
+                next_card = self.cards[i * 3 + 1]
+
+                # if suit matched and higher value (dict for comparison), update next starting player
+                if (next_card.suit is suit_to_match) and (dict_zero[next_card.value] > highest_value):
+                    next_starting_player = 1
+                    highest_value = dict_zero[next_card.value]
+                next_card = self.cards[i * 3 + 2]
+
+                # repeat (not the most elegant solution?)
+                if (next_card.suit is suit_to_match) and (dict_zero[next_card.value] > highest_value):
+                    next_starting_player = 2
+                    # highest_value = dict_zero[next_card.value]
+
+                # note the next starting player, creating a list for the 2nd - 10th round in this game
+                player_order.append(next_starting_player)
+
 
 
 
